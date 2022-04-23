ef95aff3c47e00e508fc7a203e79b0067a926d9e
srinivasn68 <103553230+srinivasn68@users.noreply.github.com>
2022-04-23 10:18:16 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,17 +1,7 @@
 
 CREATE TABLE Persons(
-PersonID int,
   LastName varchar(255),
   FirstName varchar(255),
   Address varchar(255),
   City varchar(255) 
 );
-
-Insert into Persons
-(
-  LastName, FirstName
-)
-values
-(
-  'putta','nagendra'
-)
